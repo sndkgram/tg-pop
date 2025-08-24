59feccc15502f1badfb000b376e7947d0434b5a0
sndkgram <govmail.lp@gmail.com>
2025-08-24 12:41:40 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import random
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Токен вашего бота
+TOKEN = '8474791343:AAFPXCqWDWfbixSc2DiGookvWi6fJcZyNx0'
+
+def reply_to_message(update: Update, context: CallbackContext) -> None:
+    if '?' in update.message.text:
+        response = random.choice(['Да', 'Нет'])
+        update.message.reply_text(response)
+
+def main() -> None:
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_to_message))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
